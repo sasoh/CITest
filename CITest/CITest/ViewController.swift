42083988da18841e46324778c31ba510771da33e
Alexander Popov <apopov@virtual-affairs.com>
2016-08-15 11:02:44 +0300

    Project files commit

---
+++ CITest/CITest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CITest
+//
+//  Created by Alexander Popov on 15.08.16 г..
+//  Copyright © 2016 г. -. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
